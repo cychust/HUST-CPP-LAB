f9d26855ebe830982d5649c653ef3e0bdc09df83
badcyc <13949569027@163.com>
2018-12-10 12:51:13 +0800

    add lab2

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -5,10 +5,12 @@
 
 #define MAXLINE 1000
 
+int pid[2];
+int fd[2];
 void handle(int signum) {
     if (signum == SIGINT) {
-        kill(0, SIGUSR1);
-        kill(0, SIGUSR2);
+        kill(pid[0], SIGUSR1);
+        kill(pid[1], SIGUSR2);
     }
 }
 
@@ -16,6 +18,7 @@ void handle1(int signum) {
     if (signum == SIGUSR1) {
         std::string line = "Child Process 1 is Killed by Parent!\n";
         write(STDOUT_FILENO, line.data(), strlen(line.data()));
+        close(fd[1]);
         exit(0);
     }
 }
@@ -24,6 +27,7 @@ void handle2(int signum) {
     if (signum == SIGUSR2) {
         std::string line = "Child Process 2 is Killed by Parent!\n";
         write(STDOUT_FILENO, line.data(), strlen(line.data()));
+        close(fd[0]);
         exit(0);
     }
 }
@@ -31,7 +35,7 @@ void handle2(int signum) {
 int main() {
     std::cout << "Hello, World!" << std::endl;
     int n;
-    int fd[2];
+//    int fd[2];
     pid_t pid1;
     pid_t pid2;
     int x = 1;
@@ -41,8 +45,6 @@ int main() {
     std::string message;
     std::string x_str;
     signal(SIGINT, handle);
-    signal(SIGUSR1, SIG_IGN);
-    signal(SIGUSR2, SIG_IGN);
     pipe(fd);                     //创建管道
     if ((pid1 = fork()) == -1) {   //子进程
         exit(1);
@@ -61,6 +63,7 @@ int main() {
             sleep(1);
         }
     }
+    pid[0] = pid1;
     if ((pid2 = fork()) == -1) {   //子进程2
         exit(1);
     }
@@ -76,10 +79,10 @@ int main() {
             write(STDOUT_FILENO, line, n);
         }
     }
-
-    std::cout << pid1 << " " << pid2 << std::endl;
-    waitpid(pid1, NULL, 0);
-    waitpid(pid2, NULL, 0);
+    pid[1] = pid2;
+    std::cout << pid[0] << " " << pid[1] << std::endl;
+    waitpid(pid[0], NULL, 0);
+    waitpid(pid[1], NULL, 0);
     close(fd[0]);
     close(fd[1]);
     std::cout << "Parent Process is killed" << std::endl;
---
+++ lab2/ticket.cpp
@@ -0,0 +1,106 @@
+//
+// Created by cyc on 18-12-10.
+//
+
+#include <iostream>
+
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/sem.h>
+#include <sys/ipc.h>
+#include <zconf.h>
+
+#define N 10
+union semun {
+    int val; /* value for SETVAL */
+    struct semid_ds *buf; /* buffer for IPC_STAT, IPC_SET */
+    unsigned short *array; /* array for GETALL, SETALL */
+    struct seminfo *__buf; /* buffer for IPC_INFO */
+};
+
+void P(int semId, int semNum) {
+    struct sembuf sem;
+    sem.sem_num = semNum;
+    sem.sem_op = -1;
+    sem.sem_flg = 0;
+    semop(semId, &sem, 1);
+    return;
+}
+
+struct thread_para {
+//    int a;
+    int id;         //线程id
+    int semId;
+};
+
+int sell = 0;
+int total = 100;
+
+void V(int semId, int semNum) {
+    struct sembuf sem;
+    sem.sem_num = semNum;
+    sem.sem_op = 1;
+    sem.sem_flg = 0;
+    semop(semId, &sem, 1);
+    return;
+}
+
+//int semId;
+
+void *sellHandle(void *a) {
+    struct thread_para *t = (struct thread_para *) a;
+    while (sell < total) {
+        P(t->semId, 0);
+        if (sell >= total) {
+            V(t->semId, 0);
+            break;
+        }
+        sell++;
+        std::cout << "thread " << t->id << " sell:" << "total sell is" << sell << std::endl;
+        V(t->semId, 0);
+        srand((unsigned) time(NULL));
+        usleep(rand() % 100);
+    }
+
+}
+
+
+int main() {
+//    std::cout << "Hello, World!" << std::endl;
+    union semun arg;
+    int semId;
+    int key;
+    int ret;
+    int a = 0;
+    struct thread_para para[N];                     //子线程参数
+    pthread_t thread[N];
+    void *a1[N];
+    key = ftok("/tmp", 0x66);
+    if (key < 0) {
+        perror("ftok key error");
+        return -1;
+    }
+    semId = semget(key, 1, IPC_CREAT | 0666);       //semId信号组id
+    if (semId == -1) {
+        perror("create semget error");
+        return -1;
+    }
+    arg.val = 1;                                  //信号灯 1　初值为1;
+    ret = semctl(semId, 0, SETVAL, arg);
+    if (ret < 0) {
+        perror("ctl sem error");
+        semctl(semId, 0, IPC_RMID, arg);
+        return -1;
+    }
+    for (int i = 0; i < N; ++i) {
+        para[i].semId = semId;
+        para[i].id = i + 1;
+        pthread_create(&thread[i], NULL, sellHandle, &para[i]);
+    }
+    for (int j = 0; j < N; ++j) {
+        pthread_join(thread[j], &a1[j]);
+        std::cout << "join" << j + 1 << std::endl;
+    }
+    std::cout << "main" << std::endl;
+    return 0;
+}
