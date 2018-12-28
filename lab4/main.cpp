94b8e319a5be88d2185132f46ea5ba7cacb2f9a6
cychust <u201614719@hust.edu.cn>
2018-12-28 11:01:35 +0800

    update lab4

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -8,12 +8,34 @@
 #include <pwd.h>
 #include <grp.h>
 #include <cstring>
+#include <zconf.h>
 
+//--------------------------------------------------------------------------------
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
+//---------------------------------------------------------------------------------
 
 void displayFile(char *pathName) {
     struct stat buf;
     struct tm *time;
-    char filemode[10];
+    char filemode[11];
+    char symlink[100];
     if (lstat(pathName, &buf) < 0) {
         perror("lstat error");
         return;
@@ -64,14 +86,29 @@ void displayFile(char *pathName) {
         i++;
     }
     time = localtime(&buf.st_mtime);
+    filemode[10] = '\0';
     std::cout << filemode << "  ";
     std::cout << buf.st_nlink << "  ";
     std::cout << getpwuid(buf.st_uid)->pw_name << "  ";
-    std::cout << buf.st_size << "   ";
     std::cout << getgrgid(buf.st_gid)->gr_name << "  ";
+    if (filemode[0] == 'b' || filemode[0] == 'c')
+        std::cout << (buf.st_rdev >> 8) << " " << (buf.st_rdev & 0xff);
+    else
+        std::cout << buf.st_size << "   ";
     std::cout << time->tm_year + 1900 << "-" << time->tm_mon + 1 << "-" << time->tm_mday << "  ";
     std::cout << time->tm_hour << ":" << time->tm_min << "  ";
-    std::cout << pathName << std::endl;
+
+    if (filemode[0] == 'l') {
+        memset(symlink, '\0', sizeof(symlink));
+        std::cout << BLUE << pathName;
+        if (readlink(pathName, symlink, sizeof(symlink)) > 0) {
+            symlink[strlen(symlink)] = '\0';
+            std::cout << " -> " << symlink;
+        }
+    }
+    std::cout << std::endl;
+//    else
+//        std::cout<<
 }
 
 void displayDir(char *dirName) {
@@ -95,8 +132,7 @@ void displayDir(char *dirName) {
                 continue;
             displayFile(direntp->d_name);
 //            displayDir(direntp->d_name);
-        }
-        else
+        } else
             displayFile(direntp->d_name);
     }
 
