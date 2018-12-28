fba0b15cf2a4dd78dca9f7c2492a6a9a68f777f3
cychust <u201614719@hust.edu.cn>
2018-12-28 21:13:59 +0800

    update lab4

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -9,7 +9,9 @@
 #include <grp.h>
 #include <cstring>
 #include <zconf.h>
-
+#include <list>
+#include <string>
+#include <iomanip>
 //--------------------------------------------------------------------------------
 #define RESET   "\033[0m"
 #define BLACK   "\033[30m"      /* Black */
@@ -89,41 +91,45 @@ void displayFile(char *pathName) {
     filemode[10] = '\0';
     std::cout << filemode << "  ";
     std::cout << buf.st_nlink << "  ";
-    std::cout << getpwuid(buf.st_uid)->pw_name << "  ";
-    std::cout << getgrgid(buf.st_gid)->gr_name << "  ";
+    std::cout << std::setw(4) << std::setfill(' ') << getpwuid(buf.st_uid)->pw_name << "  ";
+    std::cout << std::setw(4) << getgrgid(buf.st_gid)->gr_name << "  ";
     if (filemode[0] == 'b' || filemode[0] == 'c')
         std::cout << (buf.st_rdev >> 8) << " " << (buf.st_rdev & 0xff);
     else
-        std::cout << buf.st_size << "   ";
-    std::cout << time->tm_year + 1900 << "-" << time->tm_mon + 1 << "-" << time->tm_mday << "  ";
-    std::cout << time->tm_hour << ":" << time->tm_min << "  ";
+        std::cout << std::setw(7) << std::right << buf.st_size << "   ";
+    std::cout << time->tm_year + 1900 << "-" << time->tm_mon + 1 << "-" << time->tm_mday << "  ";   //时间
 
+    std::cout << std::setw(2) << std::setfill('0') << time->tm_hour             //时间
+              << ":"
+              << std::setw(2) << time->tm_min << "  ";
     if (filemode[0] == 'l') {
         memset(symlink, '\0', sizeof(symlink));
-        std::cout << BLUE << pathName;
+        std::cout << pathName;
         if (readlink(pathName, symlink, sizeof(symlink)) > 0) {
             symlink[strlen(symlink)] = '\0';
             std::cout << " -> " << symlink;
         }
+    } else {
+        std::cout << pathName;
     }
     std::cout << std::endl;
 //    else
 //        std::cout<<
 }
 
-void displayDir(char *dirName) {
+void displayDir(char *fileName, const char *dirName) {
+    std::list<dirent *> direntpList;
     DIR *dir;
     struct dirent *direntp;
     struct stat buf;
-    dir = opendir(dirName);
+    std::cout << dirName << ":" << std::endl;
+    dir = opendir(fileName);
     if (dir == NULL) {
         perror("error");
         return;
-
     }
-//    chdir(dirName);
+    chdir(fileName);
     while ((direntp = readdir(dir)) != NULL) {
-//        std::cout << direntp->d_name << " ";
         if (lstat(direntp->d_name, &buf) < 0) {
             perror("sss");
         }
@@ -131,34 +137,32 @@ void displayDir(char *dirName) {
             if (strcmp(direntp->d_name, "..") == 0 || strcmp(direntp->d_name, ".") == 0)
                 continue;
             displayFile(direntp->d_name);
-//            displayDir(direntp->d_name);
+            direntpList.push_back(direntp);
         } else
             displayFile(direntp->d_name);
     }
-
-//    chdir("..");
+    std::cout << std::endl;
+    for (std::list<dirent *>::iterator itr = direntpList.begin(); itr != direntpList.end(); itr++) {
+        std::string dirN = std::string(dirName) + "/" + (*itr)->d_name;
+        displayDir((*itr)->d_name, dirN.data());
+        std::cout << std::endl;
+    }
+    chdir("..");
     closedir(dir);
 }
 
 int main(int argc, char *argv[]) {
-    std::cout << "Hello, World!" << std::endl;
-//    FLAGS_logtostderr = 1;
-//    google::InitGoogleLogging(argv[0]);
-
     struct stat buf;
-    char *ptr;
     for (int i = 1; i < argc; ++i) {
         if (lstat(argv[i], &buf) < 0) {
             perror("lstat error");
             continue;
         }
         if (S_ISDIR(buf.st_mode)) {
-            ptr = "dir";
-            displayDir(argv[i]);
+            displayDir(argv[i], argv[i]);
         } else {
             displayFile(argv[i]);
         }
-        std::cout << ptr << std::endl;
     }
     return 0;
 }
